16cf3d90c02d6739a7907eea6ea75ea7c9edbb8b
OLspectre <ossi9910@gmail.com>
2025-11-21 11:17:13 +0100

    Part 3: Introduce Express
    4. Set Up Express:
    o Install Express using npm install express.
    o Rewrite the server using Express and replicate the functionality of the /,
    /about, and /contact routes
    
    *realised* that I initially wrote the syntax for the server as if it was Express. I was supposed to write it with http - the nreqrite it to express.

--- exercise_1/index.js
+++ exercise_1/index.js
@@ -1,13 +1,17 @@
 
 
-const express = require("express");
+// const express = require("express");
+// const app = express();
+
+import express from "express";
+
 const app = express();
 
 const PORT = 3333;
 
 app.get("/about", (req, res) => {
 
-    console.log("About page");
+    console.log("About page with Express");
 
 
     res.send("This is an About page.")
@@ -15,14 +19,14 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
 
-    console.log("contact page");
+    console.log("contact page With Express");
 
     res.send("This is a Contact page.")
 
 });
 
 app.get("/", (req, res) => {
-    console.log("It's friday theeeeen");
+    console.log("It's friday theeeeen with Express");
 
 
     res.send("Hello World!")
